9b02b41412aebeae243cf7c4b031cd83500233b9
shubh387 <shubham387798@gmail.com>
2025-04-23 11:21:05 +0530

    Create TestTransaction.java

---
+++ hard/TestTransaction.java
@@ -0,0 +1,14 @@
+public class MainApp { 
+public static void main(String[] args) { 
+AnnotationConfigApplicationContext ctx = new 
+AnnotationConfigApplicationContext(AppConfig.class); 
+BankService service = ctx.getBean(BankService.class); 
+try { 
+service.transferMoney(101, 102, 500); 
+System.out.println("Transaction Successful!"); 
+} catch (Exception e) { 
+System.out.println("Transaction Failed: " + e.getMessage()); 
+} 
+ctx.close(); 
+} 
+} 
