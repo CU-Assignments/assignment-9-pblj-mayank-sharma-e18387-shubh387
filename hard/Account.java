f012ad50d8629e9359db08fead666afc086199be
shubh387 <shubham387798@gmail.com>
2025-04-23 11:17:25 +0530

    Create Account.java

---
+++ hard/Account.java
@@ -0,0 +1,9 @@
+import javax.persistence.*; 
+Entity 
+public class Account { 
+@Id 
+private int accountId; 
+private String holderName; 
+private double balance; 
+// Constructors, getters, setters 
+}
